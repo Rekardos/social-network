608dbcf322f01cf5081ca07a205f95bf42fef9b2
antonmedvedev <akm3752292@yandex.ru>
2020-06-19 19:27:44 +0300

    рефакторинг authReducer.js на async await

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -9,37 +9,35 @@ export const setAuthData = (data) => {
 };
 
 
-export const setAuthThunkCreator = () => (dispatch) => {
-    AuthAPI.authMe()
-        .then(response => {
-                if (response.resultCode === 0) {
-                    dispatch(setAuthData({...response.data, isAuth: true}))
-                }
-            }
-        )
+export const setAuthThunkCreator = () => async (dispatch) => {
+    let response = await AuthAPI.authMe()
+
+    if (response.resultCode === 0) {
+        dispatch(setAuthData({...response.data, isAuth: true}))
+    }
+
+
 }
 
-export const loginThunkCreator = (email,password,rememberMe) => (dispatch) => {
+export const loginThunkCreator = (email, password, rememberMe) => async (dispatch) => {
 
-    AuthAPI.loginMe(email,password,rememberMe,false)
-        .then(response => {
-                if (response.resultCode === 0) {
-                    dispatch(setAuthThunkCreator())
-                } else {
-                    dispatch(stopSubmit('login',{_error:response.messages[0]}))
-                }
-            }
-        )
+    let response = await AuthAPI.loginMe(email, password, rememberMe, false)
+
+    if (response.resultCode === 0) {
+        dispatch(setAuthThunkCreator())
+    } else {
+        dispatch(stopSubmit('login', {_error: response.messages[0]}))
+    }
+
 }
 
-export const logOutThunkCreator = () => (dispatch) => {
-    AuthAPI.logOutMe()
-        .then(response => {
-                if (response.resultCode === 0) {
-                    dispatch(setAuthData({id: null, email: null, login: null, isAuth: false}))
-                }
-            }
-        )
+export const logOutThunkCreator = () => async (dispatch) => {
+    let response = await AuthAPI.logOutMe()
+
+    if (response.resultCode === 0) {
+        dispatch(setAuthData({id: null, email: null, login: null, isAuth: false}))
+    }
+
 }
 
 
